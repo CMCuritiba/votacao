8b47cfb2284b3fc9904169cc738106112e0d8a1a
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2018-09-12 14:58:54 -0300

    arquivos de deploy

---
+++ deploy/production/gunicorn.conf.py
@@ -0,0 +1,3 @@
+bind = 'unix:/usr/share/webapps/votacao/var/run/gunicorn.sock'
+workers = 2
+timeout = 30
---
+++ deploy/production/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd /usr/share/webapps/votacao
+
+mkdir -p /usr/share/webapps/votacao/var/run
+rm -f /usr/share/webapps/votacao/var/run/*
+
+exec /usr/share/envs/votacao/bin/gunicorn config.wsgi -c deploy/staging/gunicorn.conf.py  --env DJANGO_SETTINGS_MODULE=config.settings.production
