7e2f78d5c0bb31df6c64ea7d403449cbbccb1e50
aleodoni <aleodoni@gmail.com>
2018-10-05 14:48:46 -0300

    arquivos necessarios para rodar CI

--- config/settings/travis.py
+++ config/settings/travis.py
@@ -90,7 +90,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 ]
 
 # DEBUG
